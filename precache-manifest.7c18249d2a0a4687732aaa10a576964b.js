31e54e95675f334258da6455133347b70b12b0d8
Martín Vergara <martinmorsevergara@gmail.com>
2019-12-20 16:57:42 -0300

    Updates

---
+++ precache-manifest.7c18249d2a0a4687732aaa10a576964b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e343616d24140ab88ee141763a18b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dda6a463d795e49174f",
+    "url": "/static/css/main.1b8ac635.chunk.css"
+  },
+  {
+    "revision": "1e5b67464de83403e207",
+    "url": "/static/js/2.02433d04.chunk.js"
+  },
+  {
+    "revision": "04aa8882d17e8518fa8624df1a8b7192",
+    "url": "/static/js/2.02433d04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3dda6a463d795e49174f",
+    "url": "/static/js/main.48bf8fcd.chunk.js"
+  },
+  {
+    "revision": "e738b54ffe3b15816cf3",
+    "url": "/static/js/runtime-main.63c1efab.js"
+  }
+]);
